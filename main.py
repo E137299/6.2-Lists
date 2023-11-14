d2aaa6379d7ecac71ef999cca1da5699cd219e44
David Yin <29613693+E137299@users.noreply.github.com>
2023-11-14 05:15:22 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,32 @@
+def swap_first_and_last(my_list):
+    # Swap the first and last elements of the given list.
+    pass
+
+
+
+def replace_middle_element(my_list):
+    # Replace the middle element with the sum of all the values in the list.
+    pass
+
+
+def even_indexed_elements(my_list):
+    # Return a new list containing only the even-indexed elements of the given list.
+    pass
+
+
+def reverse_subset(my_list):
+    # Reverse the elements from index 2 to index 5 in the given list.
+    pass
+
+
+def replace_negative_numbers(my_list):
+    # Replace all negative numbers in the list with their absolute values.
+    pass
+
+
+def rotate_list_left(my_list, k):
+    # Rotate the elements of the list k positions to the left.
+    pass
+
+
+
