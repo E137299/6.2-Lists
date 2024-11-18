385a5299cd65e93db6574392b27cf7159369da4b
David Yin <29613693+E137299@users.noreply.github.com>
2024-11-18 08:35:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 # Swap the first and last elements of the given list and return the modified list
 # swap_first_and_last(["cat","dog","goldfish"]) --> ["goldfish","dog","cat"]
-# swap_first_and_last([1,2,3,4,5,6,7,8,9,10]) --> [10,2,3,4,5,6,7,8,9,1]
 def swap_first_and_last(my_list):
-    # Swap the first and last elements of the given list.
     pass
 
 # Replace the middle element with the sum of all the values in the list.
 # replace_middle_element([1,2,3,4,5]) --> [1,2,15,4,5]
-# replace_middle_element([1,2,3,4,5,6,7,8]) --> [1,2,3,4,36,6,7,8]
 def replace_middle_element(my_list):
     pass
 
@@ -15,29 +12,23 @@ def replace_middle_element(my_list):
 
 # Reverse the elements from index 2 to index 5 in the given list. Return list
 # reverse_subset([1,2,3,4,5,6,7,8,9,10]) --> [1,2,6,5,4,3,7,8,9,10]
-# reverse_subset(["Austin","Buda","Georgetown","Cedar Park","Manor","Elgin","Round Rock","Leander"]) --> ["Austin","Buda", "Elgin","Manor","Cedar Park","Georgetown","Round Rock","Leander"]
 def reverse_subset(my_list):
     pass
 
 
 # Replace all negative numbers in the list with their absolute values. Return list
 # replace_negative_numbers([-1,0,1]) --> [1,0,1]
-# replace_negative_numbers([-10,-1,0,1,-22]) --> [10,1,0,1,22]
 def replace_negative_numbers(my_list):
-    # Replace all negative numbers in the list with their absolute values.
     pass
 
 # Rotate the elements of the list k positions to the left. Return list.
 # rotate_list_left([1,2,3,4,5],1) --> [2,3,4,5,1]
-# rotate_list_left(["a","b","c","d","e","f"],3) --> ["d","e","f","a","b","c"]
 def rotate_list_left(my_list, k):
-    # Rotate the elements of the list k positions to the left.
     pass
 
 
 # Rotate the elements of the list k positions to the right Return list.
 # rotate_list_left([1,2,3,4,5],1) --> [5,1,2,3,4]
-# rotate_list_left(["a","b","c","d","e","f"],3) --> ["d","e","f","a","b","c"]
 def rotate_list_right(list, k):
     pass
 
